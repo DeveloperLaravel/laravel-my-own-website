43dd2ac74111e9c28df8c10ebf441918b5c2cb8b
Developerlaravel <hnarfr20063@github.com>
2025-07-14 12:38:16 +0200

    finshdata

---
+++ config/cors.php
@@ -0,0 +1,7 @@
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+'allowed_origins' => ['mysql-production-b07e.up.railway.app'], // أو https://your-railway-domain.railway.app فقط
+
+'allowed_methods' => ['*'],
+
+'allowed_headers' => ['*'],
